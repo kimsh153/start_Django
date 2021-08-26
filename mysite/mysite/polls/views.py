a8a70c0fde6e991b3bc7b207c97853dc90dea096
sunghun <sunghun4777@naver.com>
2021-08-26 17:35:06 +0900

    django
    뷰가 뭔가 하도록 만들기,
    404 에러 일으키기

--- mysite/mysite/polls/views.py
+++ mysite/mysite/polls/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
 
 from .models import Question
 
@@ -9,7 +12,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
@@ -23,5 +31,5 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
