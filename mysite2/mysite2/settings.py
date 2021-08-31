cc21859c0c4550c282004f1c4c44061d8d3d70e2
sunghun <sunghun4777@naver.com>
2021-08-31 17:56:33 +0900

    django
    URL과 네임 스페이스 출처:https://wikidocs.net/book/4223

--- mysite2/mysite2/settings.py
+++ mysite2/mysite2/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pybo.apps.PyboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pybo.apps.PyboConfig',
 ]
 
 MIDDLEWARE = [
